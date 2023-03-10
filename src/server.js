2e52e7cf022f1f76c2b49c5cc9d9ed48a97a96f2
Andrewij21 <andrewijaya988@gmail.com>
2023-03-10 15:41:06 +0700

    Refactor: Add compress and helmet package

--- src/server.js
+++ src/server.js
@@ -2,15 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+const helmet = require("helmet");
+const compression = require("compression");
 const routes = require("./routes/index");
 const mongoose = require("mongoose");
 
+// POST json data
 app.use(express.json());
+
+// Use LOG request
 app.use((req, res, next) => {
   console.info(req.method, req.url);
   next();
 });
-app.use(routes);
+
+app.use(routes); // Manage server route
+app.use(helmet()); // Secure HTTP headers
+app.use(
+  compression({
+    level: 9,
+  })
+); // Compress data
 
 mongoose.set("strictQuery", false);
 mongoose
