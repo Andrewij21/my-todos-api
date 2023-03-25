8de4bf7c582ea7b4b6b69435359b9c88170b18dc
Andrewij21 <andrewijaya988@gmail.com>
2023-03-25 08:02:59 +0700

    Refactor: Change cors origin

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 // ORIGIN
-const corsOptions = { origin: "http://localhost:3000" };
+const corsOptions = { origin: "https://poetic-halva-a0d5af.netlify.app/" };
 app.use(cors(corsOptions));
 
 // POST json data
